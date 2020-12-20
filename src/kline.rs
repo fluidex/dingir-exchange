3a745cf6289a574eb854a6b5ca9557b057a92c1f
HAOYUatHZ <haoyu@protonmail.com>
2020-12-20 16:49:19 +0800

    mov

--- src/kline.rs
+++ src/kline.rs
@@ -6,3 +6,26 @@ impl KlineManager {
         KlineManager {}
     }
 }
+
+
+
+// pub struct SimpleConsumerContext;
+// // TODO: impl ClientContext for SimpleConsumerContext {}
+// impl ConsumerContext for SimpleConsumerContext {
+//     // TODO:
+// }
+
+// // TODO: should we embed it into sender?
+// pub struct KafkaMessageFetcher {}
+
+// impl KafkaMessageFetcher {
+//     pub fn new(brokers: &str) -> Result<KafkaMessageFetcher> {
+//         let consumer = ClientConfig::new()
+//             .set("bootstrap.servers", brokers)
+//             .set("queue.buffering.max.ms", "1")
+//             .create_with_context(SimpleConsumerContext)?;
+//         let arc = Arc::new(consumer);
+
+//         Ok(KafkaMessageSender { consumer: arc })
+//     }
+// }
