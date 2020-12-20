24455afbebad4953c9a655d1a1d1388b594c5576
HAOYUatHZ <haoyu@protonmail.com>
2020-12-20 23:19:03 +0800

    minor

--- src/controller.rs
+++ src/controller.rs
@@ -80,6 +80,7 @@ impl Controller {
             .unwrap();
             markets.insert(entry.name.clone(), market);
         }
+        // TODO: impl copy/clone for settings
         let kafka = settings.brokers.clone();
         tokio::spawn(async move {
             KlineUpdater::run(&kafka).await;
