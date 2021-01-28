cfcdc2b49edd7e28f0aa3f27c6c57aea66fa08f1
Weifan <weifan@blackapi.com>
2021-01-28 16:20:16 +0800

    minor: adjust mocking

--- migrations/mock/20210115121017_kline.sql
+++ migrations/mock/20210115121017_kline.sql
@@ -2,12 +2,12 @@
 
 insert into trade_record select time, 'ETH_USDT', 1, price, amount, price * amount, 'ask'
 from (select time, random()*300 + 1000 as price, random()*10 as amount 
-from generate_series(now() - interval '2 day', now(), interval '1 s') as time) t;
+from generate_series(now() - interval '1 day', now() + interval '1 day', interval '1 s') as time) t;
 
 insert into trade_record select time, 'ETH_USDT', 1, price, amount, price * amount, 'ask'
 from (select time, random()*200 + 1000 as price, random()*30 as amount 
-from generate_series(now() - interval '2 day', now(), interval '3 s') as time) t;
+from generate_series(now() - interval '1 day', now() + interval '1 day', interval '3 s') as time) t;
 
 insert into trade_record select time, 'ETH_USDT', 1, price, amount, price * amount, 'ask'
 from (select time, random()*200 + 1000 as price, random()*100 as amount 
-from generate_series(now() - interval '2 day', now(), interval '15 s') as time) t;
+from generate_series(now() - interval '1 day', now() + interval '1 day', interval '15 s') as time) t;
