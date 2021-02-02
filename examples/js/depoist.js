d1a867133cef209c3b67795cffa5e1404943da6a
Weifan <weifan@blackapi.com>
2021-02-02 10:17:17 +0800

    add an deposit script so we would not enounter "balance not enough" issue

---
+++ examples/js/depoist.js
@@ -0,0 +1,11 @@
+//deposit a lot to engine, so we would not encounter "balance not enough" failure
+
+import {  depositAssets } from "./util.mjs";
+
+async function main() {
+
+  //if I really had so much money ....
+  await depositAssets({ USDT: "10000000.0", ETH: "50000.0" });
+}
+
+main().catch(console.log);
