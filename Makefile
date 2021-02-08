f268d37331775b1ce54ec4aa51604b2ca04bf4c9
lispczz <mycinbrin@gmail.com>
2021-02-08 07:35:25 +0000

    test: add more API tests to CI

--- Makefile
+++ Makefile
@@ -21,7 +21,8 @@ startall:
 	cargo build
 	mkdir -p logs
 	./target/$(BUILD_MODE)/matchengine 1>logs/matchengine.log 2>&1 &
-	./target/$(BUILD_MODE)/persistor 1>logs/persistor.log 2>&1 &
+	# fix the migrator order problem
+	sleep 3; ./target/$(BUILD_MODE)/persistor 1>logs/persistor.log 2>&1 &
 	./target/$(BUILD_MODE)/restapi 1>logs/restapi.log 2>&1 &
 pgrep:
 	pgrep -l $(PROCESSES) || true
--- examples/js/print_orders.js
+++ examples/js/print_orders.js
@@ -1,18 +1,35 @@
 import axios from "axios";
+import { strict as assert } from "assert";
+
+const isCI = !!process.env.GITHUB_ACTIONS;
+
 async function main() {
   const server = "0.0.0.0";
+  console.log("ci mode:", isCI);
   console.log("closed orders:");
-  console.log(
-    (await axios.get(`http://${server}:8765/restapi/closedorders/ETH_USDT/3`))
-      .data
-  );
+  const closedOrders = (
+    await axios.get(`http://${server}:8765/restapi/closedorders/ETH_USDT/3`)
+  ).data;
+  console.log(closedOrders);
+  if (isCI) {
+    assert.equal(closedOrders.orders.length, 3);
+  }
   console.log("active orders:");
-  console.log(
-    (await axios.get(`http://${server}:8765/api/orders/ETH_USDT/3`)).data
-  );
+  const openOrders = (
+    await axios.get(`http://${server}:8765/api/orders/ETH_USDT/3`)
+  ).data;
+  console.log(openOrders);
+  if (isCI) {
+    assert.equal(openOrders.orders.length, 1);
+  }
   console.log("market ticker:");
-  console.log(
-    (await axios.get(`http://${server}:8765/restapi/ticker_24h/ETH_USDT`)).data
-  );
+  const ticker = (
+    await axios.get(`http://${server}:8765/restapi/ticker_24h/ETH_USDT`)
+  ).data;
+  console.log(ticker);
+  if (isCI) {
+    assert.equal(ticker.volume, 4);
+    assert.equal(ticker.quote_volume, 4.4);
+  }
 }
 main().catch(console.log);
