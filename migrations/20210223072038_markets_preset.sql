9df4cdbf785f541862c1055291aa5b364bb66d9d
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 16:28:14 +0800

    fix market initiation

--- migrations/20210223072038_markets_preset.sql
+++ migrations/20210223072038_markets_preset.sql
@@ -8,6 +8,6 @@ insert into asset (symbol, name, token_address, is_commonly_quoted, precision_st
 	values ('USDT', 'Tether USD', '0xdAC17F958D2ee523a2206206994597C13D831ec7', true, 6, 6);
 
 insert into market (base_asset, quote_asset, precision_base, precision_quote, precision_fee, min_amount) values 
-('ETH', 'USDT', 4, 2, 2, 0.001)
-,('UNI', 'USDT', 4, 2, 2, 0.001)
+('0x0000000000000000000000000000000000000000', '0xdAC17F958D2ee523a2206206994597C13D831ec7', 4, 2, 2, 0.001)
+,('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', '0xdAC17F958D2ee523a2206206994597C13D831ec7', 4, 2, 2, 0.001)
 ;
