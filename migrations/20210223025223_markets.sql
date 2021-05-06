d70612d7b2363ae44790eeee1147e75c50951954
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 14:57:58 +0800

    upgrade "asset" db schema

--- migrations/20210223025223_markets.sql
+++ migrations/20210223025223_markets.sql
@@ -1,9 +1,14 @@
 -- Add migration script here
 CREATE TABLE asset (
-    asset_name VARCHAR(30) PRIMARY KEY,
+    symbol VARCHAR(30),
+    name VARCHAR(30),
+    chainId SMALLINT DEFAULT 1,
+    token_address VARCHAR(64),
     precision_stor SMALLINT CHECK (precision_stor >= 0) NOT NULL,
     precision_show SMALLINT CHECK (precision_show >= 0) NOT NULL,
-    create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP
+    logoURI VARCHAR(256),
+    create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (token_adress)
 );
 
 CREATE TABLE market (
