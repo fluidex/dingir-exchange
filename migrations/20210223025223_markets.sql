9dff96f0708d9314c764b6961f5df98250510ded
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 16:27:11 +0800

    fix foreign

--- migrations/20210223025223_markets.sql
+++ migrations/20210223025223_markets.sql
@@ -2,14 +2,12 @@
 CREATE TABLE asset (
     symbol VARCHAR(30) NOT NULL DEFAULT '',
     name VARCHAR(30) NOT NULL DEFAULT '',
-    chain_id SMALLINT DEFAULT 1,
-    token_address VARCHAR(64) NOT NULL,
+    token_address VARCHAR(64) NOT NULL PRIMARY KEY,
     is_commonly_quoted BOOLEAN DEFAULT false,
     precision_stor SMALLINT CHECK (precision_stor >= 0) NOT NULL,
     precision_show SMALLINT CHECK (precision_show >= 0) NOT NULL,
     logo_uri VARCHAR(256) NOT NULL DEFAULT '',
-    create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (chain_id, token_address)
+    create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX asset_idx_symbol ON asset (symbol);
@@ -19,8 +17,8 @@ CREATE INDEX asset_idx_token_address ON asset (token_address);
 CREATE TABLE market (
     id SERIAL PRIMARY KEY,
     create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
-    base_asset VARCHAR(30) NOT NULL REFERENCES asset(symbol) ON DELETE RESTRICT, -- TODO: USE address
-    quote_asset VARCHAR(30) NOT NULL REFERENCES asset(symbol) ON DELETE RESTRICT, -- TODO: USE address
+    base_asset VARCHAR(64) NOT NULL REFERENCES asset(token_address) ON DELETE RESTRICT,
+    quote_asset VARCHAR(64) NOT NULL REFERENCES asset(token_address) ON DELETE RESTRICT,
     precision_base SMALLINT CHECK (precision_base >= 0) NOT NULL,
     precision_quote SMALLINT CHECK (precision_quote >= 0) NOT NULL,
     precision_fee SMALLINT CHECK (precision_fee >= 0) NOT NULL,
