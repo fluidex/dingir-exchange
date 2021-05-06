fff976c761c045e7a09ee5d9b229476bc15d5b57
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 16:13:55 +0800

    minor

--- migrations/20210223025223_markets.sql
+++ migrations/20210223025223_markets.sql
@@ -1,13 +1,13 @@
 -- Add migration script here
 CREATE TABLE asset (
-    symbol VARCHAR(30) NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    chainId SMALLINT DEFAULT 1,
+    symbol VARCHAR(30) NOT NULL DEFAULT '',
+    name VARCHAR(30) NOT NULL DEFAULT '',
+    chain_id SMALLINT DEFAULT 1,
     token_address VARCHAR(64) NOT NULL,
     is_commonly_quoted BOOLEAN DEFAULT false,
     precision_stor SMALLINT CHECK (precision_stor >= 0) NOT NULL,
     precision_show SMALLINT CHECK (precision_show >= 0) NOT NULL,
-    logoURI VARCHAR(256),
+    logo_uri VARCHAR(256) NOT NULL DEFAULT '',
     create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (chainId, token_address)
 );
