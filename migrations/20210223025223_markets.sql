0b35b0d812f06eb731018a2dc6429bf7056c9ebf
HAOYUatHZ <haoyu@protonmail.com>
2021-05-08 06:48:38 +0800

    upgrade db schemas

--- migrations/20210223025223_markets.sql
+++ migrations/20210223025223_markets.sql
@@ -1,9 +1,12 @@
 -- Add migration script here
 CREATE TABLE asset (
-    symbol VARCHAR(30) NOT NULL PRIMARY KEY,
+    id VARCHAR(64) NOT NULL PRIMARY KEY,
+    symbol VARCHAR(30) NOT NULL DEFAULT '',
     name VARCHAR(30) NOT NULL DEFAULT '',
     chain_id SMALLINT CHECK (chain_id >= 0) NOT NULL DEFAULT 1,
     token_address VARCHAR(64) NOT NULL DEFAULT '',
+    -- token_address VARCHAR(64) DEFAULT NULL,
+    -- UNIQUE (chain_id, token_address),
     precision_stor SMALLINT CHECK (precision_stor >= 0) NOT NULL,
     precision_show SMALLINT CHECK (precision_show >= 0) NOT NULL,
     logo_uri VARCHAR(256) NOT NULL DEFAULT '',
@@ -13,8 +16,8 @@ CREATE TABLE asset (
 CREATE TABLE market (
     id SERIAL PRIMARY KEY,
     create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
-    base_asset VARCHAR(30) NOT NULL REFERENCES asset(symbol) ON DELETE RESTRICT,
-    quote_asset VARCHAR(30) NOT NULL REFERENCES asset(symbol) ON DELETE RESTRICT,
+    base_asset VARCHAR(30) NOT NULL REFERENCES asset(id) ON DELETE RESTRICT,
+    quote_asset VARCHAR(30) NOT NULL REFERENCES asset(id) ON DELETE RESTRICT,
     precision_base SMALLINT CHECK (precision_base >= 0) NOT NULL,
     precision_quote SMALLINT CHECK (precision_quote >= 0) NOT NULL,
     precision_fee SMALLINT CHECK (precision_fee >= 0) NOT NULL,
--- migrations/20210223072038_markets_preset.sql
+++ migrations/20210223072038_markets_preset.sql
@@ -1,11 +1,11 @@
 -- Add migration script here
 
-insert into asset (symbol, name, token_address, precision_stor, precision_show) 
-	values ('ETH', 'Ether', '0x0000000000000000000000000000000000000000', 6, 6);
-insert into asset (symbol, name, token_address, precision_stor, precision_show) 
-	values ('UNI', 'Uniswap', '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', 6, 6);
-insert into asset (symbol, name, token_address, precision_stor, precision_show) 
-	values ('USDT', 'Tether USD', '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 6);
+insert into asset (id, symbol, name, token_address, precision_stor, precision_show) 
+	values ('ETH', 'ETH', 'Ether', '', 6, 6);
+insert into asset (id, symbol, name, token_address, precision_stor, precision_show) 
+	values ('UNI', 'UNI', 'Uniswap', '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', 6, 6);
+insert into asset (id, symbol, name, token_address, precision_stor, precision_show) 
+	values ('USDT', 'USDT', 'Tether USD', '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 6);
 
 insert into market (base_asset, quote_asset, precision_base, precision_quote, precision_fee, min_amount) values 
 ('ETH', 'USDT', 4, 2, 2, 0.001)
