83a01595297ffedc3efb28d9a5289b10a8e26c69
HAOYUatHZ <haoyu@protonmail.com>
2021-05-14 16:45:35 +0800

    fix schema

--- migrations/20210514140412_account.sql
+++ migrations/20210514140412_account.sql
@@ -3,7 +3,7 @@
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
     l1_address VARCHAR(64) NOT NULL DEFAULT '',
-    l2_address VARCHAR(64) NOT NULL DEFAULT '',
+    l2_address VARCHAR(64) NOT NULL DEFAULT ''
 );
 
 CREATE INDEX account_l1_address ON account (l1_address);
