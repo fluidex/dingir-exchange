e2dcff7cbc87c561e5d08895084c4ddf9a4dd0c5
HAOYUatHZ <haoyu@protonmail.com>
2021-05-14 14:55:13 +0800

    move get_user

--- src/bin/restapi.rs
+++ src/bin/restapi.rs
@@ -21,32 +21,10 @@ use restapi::public_history::{order_trades, recent_trades};
 use restapi::state::{AppCache, AppState};
 use restapi::tradingview::{chart_config, history, search_symbols, symbols, ticker, unix_timestamp};
 use restapi::types::UserInfo;
+use restapi::user::get_user;
 
 async fn ping(_req: HttpRequest, _data: web::Data<AppState>) -> impl Responder {
     "pong"
-}
-
-async fn get_user(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
-    let is_debug: bool = req.match_info().get("debug").unwrap_or(false);
-    if is_debug {
-        let user_id = req.match_info().get("id_or_addr").unwrap();
-        if user_id.starts_with("0x") {
-            let mut user_map = data.user_addr_map.lock().unwrap();
-            if !user_map.contains_key(user_id) {
-                // TODO: real query from DB
-                let count = user_map.len();
-                user_map.insert(user_id.to_string(), UserInfo { user_id: count as i64 });
-            }
-            let user_info = *user_map.get(user_id).unwrap();
-            Ok(web::Json(user_info))
-        } else {
-            // TODO: get_by_user_id still fails
-            Err(RpcError::bad_request("invalid user id or address"))
-        }
-    } else {
-        // TODO:
-        Err(RpcError::bad_request("must debug mode"))
-    }
 }
 
 #[actix_web::main]
---
+++ src/restapi/user.rs
@@ -0,0 +1,27 @@
+use super::errors::RpcError;
+use super::state::AppState;
+use super::types::UserInfo;
+use actix_web::{web, HttpRequest, Responder};
+
+async fn get_user(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let is_debug: bool = req.match_info().get("debug").unwrap_or(false);
+    if is_debug {
+        let user_id = req.match_info().get("id_or_addr").unwrap();
+        if user_id.starts_with("0x") {
+            let mut user_map = data.user_addr_map.lock().unwrap();
+            if !user_map.contains_key(user_id) {
+                // TODO: real query from DB
+                let count = user_map.len();
+                user_map.insert(user_id.to_string(), UserInfo { user_id: count as i64 });
+            }
+            let user_info = *user_map.get(user_id).unwrap();
+            Ok(web::Json(user_info))
+        } else {
+            // TODO: get_by_user_id still fails
+            Err(RpcError::bad_request("invalid user id or address"))
+        }
+    } else {
+        // TODO:
+        Err(RpcError::bad_request("must debug mode"))
+    }
+}
