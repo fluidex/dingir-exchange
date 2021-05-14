e1b3ca678077a1e81ae054f0ddbbe63e2d820873
HAOYUatHZ <haoyu@protonmail.com>
2021-05-14 16:07:27 +0800

    fix query

--- src/restapi/user.rs
+++ src/restapi/user.rs
@@ -12,12 +12,12 @@ pub async fn get_user(req: HttpRequest, data: web::Data<AppState>) -> impl Respo
         is_debug = true;
     }
 
+    let user_id: &str = req.match_info().get("id_or_addr").unwrap();
+
     if is_debug {
-        let user_id = req.match_info().get("id_or_addr").unwrap();
         if user_id.starts_with("0x") {
             let mut user_map = data.user_addr_map.lock().unwrap();
             if !user_map.contains_key(user_id) {
-                // TODO: real query from DB
                 let count = user_map.len();
                 user_map.insert(
                     user_id.to_string(),
@@ -36,9 +36,15 @@ pub async fn get_user(req: HttpRequest, data: web::Data<AppState>) -> impl Respo
         }
     } else {
         // TODO: this API result should be cached, either in-memory or using redis
-        let sql_query = format!("select * from {} where market = $1 order by time desc", USER);
+        let sql_query = format!("select * from {} where id = $1 OR l1_address = $2 OR l2_address = $3", USER);
         // TODO: fecth_one? fecth_optional?
-        let user: UserDesc = sqlx::query_as(&sql_query).fetch_one(&data.db).await?;
+        let user: UserDesc = sqlx::query_as(&sql_query)
+            .bind(user_id)
+            .bind(user_id)
+            .bind(user_id)
+            .fetch_one(&data.db)
+            .await
+            .map_err(|_| RpcError::bad_request("invalid user id or address"))?;
         Ok(Json(user))
     }
 }
