7cbbbb35c7979f2cd2016a5ceaef2b51b6b4ea52
HAOYUatHZ <haoyu@protonmail.com>
2021-05-26 13:48:14 +0800

    fix

--- src/message/persist.rs
+++ src/message/persist.rs
@@ -666,8 +666,8 @@ impl<'r> From<&'r super::UserMessage> for models::AccountDesc {
     fn from(origin: &'r super::UserMessage) -> Self {
         Self {
             id: origin.user_id as i32,
-            l1_address: origin.l1_address,
-            l2_pubkey: origin.l2_pubkey,
+            l1_address: origin.l1_address.clone(),
+            l2_pubkey: origin.l2_pubkey.clone(),
         }
     }
 }
