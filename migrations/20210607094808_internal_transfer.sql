d9ab58727d1f61708a222b51347b3bc9f1ca48dd
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-06-07 10:40:18 +0800

    DB: add internal_tx migration script (#166)
    * init internal_tx migration script
    
    * clean up
    
    * fix

---
+++ migrations/20210607094808_internal_transfer.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+CREATE TABLE internal_tx (
+    time TIMESTAMP(0) NOT NULL,
+    user_from INT CHECK (user_from >= 0) NOT NULL,
+    user_to INT CHECK (user_from >= 0) NOT NULL,
+    asset VARCHAR(30) NOT NULL REFERENCES asset(id),
+    amount DECIMAL(30, 8) NOT NULL
+);
+
+CREATE INDEX internal_tx_idx_to_time ON internal_tx (user_to, time DESC);
+CREATE INDEX internal_tx_idx_from_time ON internal_tx (user_from, time DESC);
+
+SELECT create_hypertable('internal_tx', 'time');
