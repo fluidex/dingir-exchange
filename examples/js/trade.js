17914ac5b225fef10ad1a7d4831c4e1a200995db
lispczz <mycinbrin@gmail.com>
2021-06-09 10:25:23 +0000

    fix

--- examples/js/trade.js
+++ examples/js/trade.js
@@ -148,11 +148,18 @@ async function testStatusAfterTrade(askOrderId, bidOrderId) {
   const depth = await orderDepth(market, 100, /*not merge*/ "0");
   //assert.deepEqual(depth, { asks: [], bids: [{ price: "1.1", amount: "6" }] });
   //assert.deepEqual(depth, { asks: [], bids: [{ price: "1.1", amount: "6" }] });
+  // 4 * 1.1 sell, filled 4
   const balance1 = await balanceQuery(askUser);
-  decimalEqual(balance1.USDT.available, "93.4");
-  decimalEqual(balance1.USDT.frozen, "6.6");
-  decimalEqual(balance1.ETH.available, "50");
+  decimalEqual(balance1.USDT.available, "104.4");
+  decimalEqual(balance1.USDT.frozen, "0");
+  decimalEqual(balance1.ETH.available, "46");
   decimalEqual(balance1.ETH.frozen, "0");
+  // 10 * 1.1 buy, filled 4
+  const balance2 = await balanceQuery(bidUser);
+  decimalEqual(balance2.USDT.available, "89");
+  decimalEqual(balance2.USDT.frozen, "6.6");
+  decimalEqual(balance2.ETH.available, "54");
+  decimalEqual(balance2.ETH.frozen, "0");
 }
 
 async function simpleTest() {
