bca54a3c93d71c909e3ee83340ea10b069ff31e8
HAOYUatHZ <haoyu@protonmail.com>
2021-06-11 17:40:48 +0800

    try rename

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn build_grpc() {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .field_attribute(".matchengine.AssetListResponse.AssetInfo.chain_id", "#[serde(rename = \"chainId\")]")
+        .field_attribute(".matchengine.AssetListResponse.AssetInfo.token_address", "#[serde(rename = \"address\")]")
+        .field_attribute(".matchengine.AssetListResponse.AssetInfo.logo_uri", "#[serde(rename = \"logoURI\")]")
         .compile(
             &["proto/exchange/matchengine.proto"],
             &["proto/exchange", "proto/third_party/googleapis"],
