0c8f7336e7b5f4349535c0fb2728d33d4c77d4a5
HAOYUatHZ <haoyu@protonmail.com>
2021-06-23 14:26:13 +0800

    minor

--- src/matchengine/persist/state_save_load.rs
+++ src/matchengine/persist/state_save_load.rs
@@ -135,6 +135,10 @@ pub async fn load_slice_from_db(conn: &mut ConnectionType, slice_id: i64, contro
             .await
             .unwrap();
         for order in &orders {
+            if order.post_only {
+                continue;
+            }
+
             let market = controller.markets.get_mut(&order.market).unwrap();
             let order = Order {
                 id: order.id as u64,
@@ -157,7 +161,6 @@ pub async fn load_slice_from_db(conn: &mut ConnectionType, slice_id: i64, contro
                 finished_fee: order.finished_fee,
                 post_only: order.post_only,
             };
-            // TODO: deal with post_only?
             market.insert_order_into_orderbook(order);
         }
         if let Some(last_order) = orders.last() {
