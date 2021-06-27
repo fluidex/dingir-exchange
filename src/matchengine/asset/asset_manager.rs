2a3ab9f39b0afc07814f4ccf167405b77f0b61c8
HAOYUatHZ <haoyu@protonmail.com>
2021-06-27 13:07:49 +0800

    minor

--- src/matchengine/asset/asset_manager.rs
+++ src/matchengine/asset/asset_manager.rs
@@ -1,7 +1,7 @@
 use crate::config;
 use crate::market::OrderCommitment;
 use crate::matchengine::rpc::*;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -63,8 +63,28 @@ impl AssetManager {
         self.asset_get(id).unwrap().prec_show
     }
 
+    // message OrderPutRequest {
+    //   uint32 user_id = 1;
+    //   string market = 2;
+    //   OrderSide order_side = 3;
+    //   OrderType order_type = 4;
+    //   string amount = 5; // always amount for base, even for market bid
+    //   string price = 6;  // should be empty or zero for market order
+    //   string quote_limit = 7; // onyl valid for market bid order
+    //   string taker_fee = 8;
+    //   string maker_fee = 9;
+    //   bool post_only = 10; // Ensures an Limit order is only subject to Maker Fees (ignored for Market orders).
+    //   string signature = 11; // bjj signature used in Fluidex
+    // }
+
+    // impl TryFrom<OrderPutRequest> for market::OrderInput {
     // pub fn exchange_order_to_rollup_order
     pub fn commit_order(&self, o: &OrderPutRequest) -> Result<OrderCommitment> {
+        let assets: Vec<&str> = o.market.split('_').collect();
+        if assets.len() != 2 {
+            return Err(anyhow!("market error"));
+        }
+
         unimplemented!()
     }
 }
