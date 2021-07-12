cc1c00308f432a8f781ba3e589b07a43f327f7b1
lightsing <light.tsing@gmail.com>
2021-07-12 14:36:21 +0800

    apply cargo fmt

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
 pub mod timeutil;
 pub use timeutil::*;
-pub mod strings;
 pub mod serde;
+pub mod strings;
 
 pub use strings::*;
--- src/utils/serde.rs
+++ src/utils/serde.rs
@@ -1,39 +1,50 @@
 use core::fmt;
 use core::marker::PhantomData;
+use serde::de::{Deserializer, Error, Unexpected, Visitor};
 use serde::ser::Serializer;
-use serde::de::{Deserializer, Visitor, Error, Unexpected};
 use std::convert::TryInto;
 
 pub trait HexArray<'de>: Sized {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer;
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de>;
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer;
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>;
 }
 
-impl <'de, const N: usize> HexArray<'de> for [u8; N] {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+impl<'de, const N: usize> HexArray<'de> for [u8; N] {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
         serializer.serialize_str(hex::encode(&self).as_str())
     }
 
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
-
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
         struct HexArrayVisitor<T> {
-            value: PhantomData<T>
+            value: PhantomData<T>,
         }
 
-        impl <'de, const N: usize> Visitor<'de> for HexArrayVisitor<[u8; N]> {
+        impl<'de, const N: usize> Visitor<'de> for HexArrayVisitor<[u8; N]> {
             type Value = [u8; N];
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
                 write!(formatter, "an hex encoded array of length {}", N)
             }
 
-            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: Error {
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
                 hex::decode(v)
                     .ok()
                     .and_then(|v| v.try_into().ok())
                     .ok_or_else(|| Error::invalid_type(Unexpected::Str(v), &self))
             }
-
         }
 
         let visitor = HexArrayVisitor { value: PhantomData };
