35ce125c6e258a63234520c8d6e0f5d0e21f13b8
lispczz <mycinbrin@gmail.com>
2021-08-26 08:50:43 +0000

    trading: more log for bots

--- examples/js/bots/run_bots.ts
+++ examples/js/bots/run_bots.ts
@@ -142,7 +142,7 @@ async function initUser(): Promise<number> {
     "camp awful sand include refuse cash reveal mystery pupil salad length plunge square admit vocal draft found side same clock hurt length say figure";
   const mnemonic3 =
     "sound select report rug run cave provide index grief foster bar someone garage donate nominee crew once oil sausage flight tail holiday style afford";
-  const acc = Account.fromMnemonic(mnemonic1);
+  const acc = Account.fromMnemonic(mnemonic2);
   //console.log('acc is', acc);
   const restClient = defaultRESTClient;
   let userInfo = await restClient.get_user_by_addr(acc.ethAddr);
@@ -215,128 +215,134 @@ async function main() {
   let count = 0;
   while (true) {
     //console.log("count:", count);
-    if (count % 100 == 0) {
-      const externalPrice = await getPriceOfCoin(baseCoin);
-      console.log("externalPrice:", externalPrice);
-      console.log("depth:", await defaultClient.orderDepth(market, 20, "0.1"));
-      console.log("orders:", await defaultClient.orderQuery(user_id, market));
-      await defaultGrpcClient.orderCancelAll(user_id, market);
-
-      async function printBalance() {
-        const balance = await defaultGrpcClient.balanceQuery(user_id);
-        const allBase =
-          Number(balance.get(baseCoin).available) +
-          Number(balance.get(baseCoin).frozen);
-        const allQuote =
-          Number(balance.get(quoteCoin).available) +
-          Number(balance.get(quoteCoin).frozen);
-
-        let res = await estimateMarketOrderSell(
-          defaultGrpcClient,
-          market,
-          allBase
-        );
+    try {
+      if (count % 100 == 0) {
+        const externalPrice = await getPriceOfCoin(baseCoin);
+        console.log("externalPrice:", externalPrice);
         console.log(
-          "------- BALANCE1:",
-          allQuote,
-          res.quote,
-          allQuote + res.quote
+          "depth:",
+          await defaultClient.orderDepth(market, 20, "0.1")
         );
-        console.log(
-          "------- BALANCE2:",
-          allQuote,
-          allBase * externalPrice,
-          allQuote + allBase * externalPrice
-        );
-      }
+        console.log("orders:", await defaultClient.orderQuery(user_id, market));
+        await defaultGrpcClient.orderCancelAll(user_id, market);
 
-      //console.log("before rebalance");
-      await printBalance();
-      /*
+        async function printBalance() {
+          const balance = await defaultGrpcClient.balanceQuery(user_id);
+          const allBase =
+            Number(balance.get(baseCoin).available) +
+            Number(balance.get(baseCoin).frozen);
+          const allQuote =
+            Number(balance.get(quoteCoin).available) +
+            Number(balance.get(quoteCoin).frozen);
+
+          let res = await estimateMarketOrderSell(
+            defaultGrpcClient,
+            market,
+            allBase
+          );
+          console.log("external base price", externalPrice);
+          console.log("------- BALANCE1:", {
+            quote: allQuote,
+            base: res.quote,
+            total: allQuote + res.quote
+          });
+          console.log("------- BALANCE2:", {
+            quote: allQuote,
+            base: allBase * externalPrice,
+            total: allQuote + allBase * externalPrice,
+            totalInB: allQuote / externalPrice + allBase
+          });
+        }
+
+        //console.log("before rebalance");
+        await printBalance();
+        /*
       const relanced = await rebalance(user_id);
       if (relanced) {
         console.log("after rebalance");
         await printBalance();
       }
       */
-    }
-    let orderbook = null; // fetch orderbook
-    sleep(500);
-    // put a big buy order and a big sell order
-    const price = await getPriceOfCoin(baseCoin, 5);
-
-    const balance = await defaultGrpcClient.balanceQuery(user_id);
-    const allBase =
-      Number(balance.get(baseCoin).available) +
-      Number(balance.get(baseCoin).frozen);
-    const allQuote =
-      Number(balance.get(quoteCoin).available) +
-      Number(balance.get(quoteCoin).frozen);
-    //console.log({allBase, allQuote});
-    const ratio = 0.8; // use 80% of my assets to make market
-
-    const spread = 0.0005;
-    const askPrice = price * (1 + spread);
-    const bidPrice = price * (1 - spread);
-    const bidAmount = (allQuote * ratio) / bidPrice;
-    const askAmount = allBase * ratio;
-
-    // const { user_id, market, order_side, order_type, amount, price, taker_fee, maker_fee } = o;
-    const bid_order = {
-      user_id,
-      market,
-      order_side: ORDER_SIDE_BID,
-      order_type: ORDER_TYPE_LIMIT,
-      amount: bidAmount,
-      price: bidPrice
-    };
-    const ask_order = {
-      user_id,
-      market,
-      order_side: ORDER_SIDE_ASK,
-      order_type: ORDER_TYPE_LIMIT,
-      amount: askAmount,
-      price: askPrice
-    };
-
-    await defaultGrpcClient.orderCancelAll(user_id, market);
-    //console.log('batch orders:', orders);
-    try {
-      if (bidAmount > 0.001) {
-        await defaultClient.orderPut(
-          user_id,
-          market,
-          ORDER_SIDE_BID,
-          ORDER_TYPE_LIMIT,
-          bidAmount,
-          bidPrice,
-          "0",
-          "0"
-        );
       }
-    } catch (e) {
-      console.log("put error", bid_order, e);
-    }
-    try {
-      if (askAmount > 0.001) {
-        await defaultClient.orderPut(
-          user_id,
-          market,
-          ORDER_SIDE_ASK,
-          ORDER_TYPE_LIMIT,
-          askAmount,
-          askPrice,
-          "0",
-          "0"
-        );
-      }
-    } catch (e) {
-      console.log("put error", ask_order, e);
-    }
-    //let batchPutResult = await defaultClient.batchOrderPut(market, true, orders);
-    //console.log('batch put result:', batchPutResult)
+      let orderbook = null; // fetch orderbook
+      // put a big buy order and a big sell order
+      const price = await getPriceOfCoin(baseCoin, 5);
 
+      const balance = await defaultGrpcClient.balanceQuery(user_id);
+      const allBase =
+        Number(balance.get(baseCoin).available) +
+        Number(balance.get(baseCoin).frozen);
+      const allQuote =
+        Number(balance.get(quoteCoin).available) +
+        Number(balance.get(quoteCoin).frozen);
+      //console.log({allBase, allQuote});
+      const ratio = 0.8; // use 80% of my assets to make market
+
+      const spread = 0.0005;
+      const askPrice = price * (1 + spread);
+      const bidPrice = price * (1 - spread);
+      const bidAmount = (allQuote * ratio) / bidPrice;
+      const askAmount = allBase * ratio;
+
+      // const { user_id, market, order_side, order_type, amount, price, taker_fee, maker_fee } = o;
+      const bid_order = {
+        user_id,
+        market,
+        order_side: ORDER_SIDE_BID,
+        order_type: ORDER_TYPE_LIMIT,
+        amount: bidAmount,
+        price: bidPrice
+      };
+      const ask_order = {
+        user_id,
+        market,
+        order_side: ORDER_SIDE_ASK,
+        order_type: ORDER_TYPE_LIMIT,
+        amount: askAmount,
+        price: askPrice
+      };
+
+      await defaultGrpcClient.orderCancelAll(user_id, market);
+      //console.log('batch orders:', orders);
+      try {
+        if (bidAmount > 0.001) {
+          await defaultClient.orderPut(
+            user_id,
+            market,
+            ORDER_SIDE_BID,
+            ORDER_TYPE_LIMIT,
+            bidAmount,
+            bidPrice,
+            "0",
+            "0"
+          );
+        }
+      } catch (e) {
+        console.log("put error", bid_order, e);
+      }
+      try {
+        if (askAmount > 0.001) {
+          await defaultClient.orderPut(
+            user_id,
+            market,
+            ORDER_SIDE_ASK,
+            ORDER_TYPE_LIMIT,
+            askAmount,
+            askPrice,
+            "0",
+            "0"
+          );
+        }
+      } catch (e) {
+        console.log("put error", ask_order, e);
+      }
+      //let batchPutResult = await defaultClient.batchOrderPut(market, true, orders);
+      //console.log('batch put result:', batchPutResult)
+    } catch (e) {
+      console.log("err", e);
+    }
     count += 1;
+    sleep(500);
   }
 }
 
