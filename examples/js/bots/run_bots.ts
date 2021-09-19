3dd4fb96b5f38b9cd63f0ffc222c98ef8c4f390e
lispczz <mycinbrin@gmail.com>
2021-09-19 10:09:45 +0000

    trading: log more stats

--- examples/js/bots/run_bots.ts
+++ examples/js/bots/run_bots.ts
@@ -1,5 +1,5 @@
 import { MMByPriceBot } from "./mm_external_price_bot";
-
+import * as regression from "regression";
 import { Account } from "fluidex.js";
 import { defaultRESTClient, RESTClient } from "../RESTClient";
 import { defaultClient as defaultGrpcClient, Client as grpcClient, defaultClient } from "../client";
@@ -12,9 +12,14 @@ import {
   execMarketOrderAsLimit_Buy,
   rebalance,
   printBalance,
+  totalBalance,
 } from "./utils";
 import { executeOrders } from "./executor";
 import { depositAssets, getPriceOfCoin } from "../exchange_helper";
+
+//const VERBOSE = false;
+console.log({ VERBOSE });
+
 async function initUser(): Promise<number> {
   const mnemonic1 = "split logic consider degree smile field term style opera dad believe indoor item type beyond";
   const mnemonic2 =
@@ -64,7 +69,10 @@ async function main() {
   bot.priceFn = async function (coin: string) {
     return await getPriceOfCoin(coin, 5, "coinstats");
   };
+  let balanceStats = [];
   let count = 0;
+  const startTime = Date.now() / 1000;
+  const { totalValue: totalValueWhenStart } = await totalBalance(user_id, baseCoin, quoteCoin, market);
   while (true) {
     if (VERBOSE) {
       console.log("count:", count);
@@ -76,11 +84,23 @@ async function main() {
     await sleep(500);
     try {
       if (count % 100 == 1) {
+        const t = Date.now() / 1000; // ms
         console.log("stats of", bot.name);
         console.log("orders:");
         console.log(await defaultClient.orderQuery(user_id, market));
         console.log("balances:");
         await printBalance(user_id, baseCoin, quoteCoin, market);
+        let { totalValue } = await totalBalance(user_id, baseCoin, quoteCoin, market);
+        balanceStats.push([t, totalValue]);
+        if (balanceStats.length >= 2) {
+          const pastHour = (t - startTime) / 3600;
+          const assetRatio = totalValue / totalValueWhenStart;
+          console.log("time(hour)", pastHour, "asset ratio", assetRatio);
+          console.log("current ROI per hour:", (assetRatio - 1) / pastHour);
+          // we should use exp regression rather linear
+          const hourDelta = 3600 * regression.linear(balanceStats).equation[0];
+          console.log("regression ROI per hour:", hourDelta / totalValueWhenStart);
+        }
       }
 
       const oldOrders = await defaultClient.orderQuery(user_id, market);
@@ -91,7 +111,7 @@ async function main() {
       const balance = await defaultClient.balanceQuery(user_id);
       const { reset, orders } = await bot.tick(balance, oldOrders);
 
-      await executeOrders(defaultClient, market, user_id, reset, orders, 0.001, true);
+      await executeOrders(defaultClient, market, user_id, reset, orders, 0.001, false);
     } catch (e) {
       console.log("err", e);
     }
