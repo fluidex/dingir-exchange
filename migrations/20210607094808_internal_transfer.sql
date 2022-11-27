65bdddbc366390b5f459ce38071dbc3db9b32c96
towhid <t.zeinali@yourypto.com>
2022-11-27 18:54:58 +0330

    increase varchar size

--- migrations/20210607094808_internal_transfer.sql
+++ migrations/20210607094808_internal_transfer.sql
@@ -2,11 +2,11 @@
 CREATE TABLE internal_tx (
     time TIMESTAMP(0) NOT NULL,
     user_from INT CHECK (user_from >= 0) NOT NULL,
-    from_broker_id VARCHAR(100) NOT NULL,
-    from_account_id VARCHAR(100) NOT NULL,
+    from_broker_id VARCHAR(40) NOT NULL,
+    from_account_id VARCHAR(40) NOT NULL,
     user_to INT CHECK (user_to >= 0) NOT NULL,
-    to_broker_id VARCHAR(100) NOT NULL,
-    to_account_id VARCHAR(100) NOT NULL,
+    to_broker_id VARCHAR(40) NOT NULL,
+    to_account_id VARCHAR(40) NOT NULL,
     asset VARCHAR(30) NOT NULL REFERENCES asset(id),
     amount DECIMAL(30, 8) CHECK (amount > 0) NOT NULL,
     signature BYTEA NOT NULL
