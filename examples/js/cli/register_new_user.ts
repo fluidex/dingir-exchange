7a90bdd9eaa599ddba87bc198590f1e492007a6e
towhid <t.zeinali@yourypto.com>
2022-12-07 12:11:56 +0330

    change user id in register new user into string

--- examples/js/cli/register_new_user.ts
+++ examples/js/cli/register_new_user.ts
@@ -6,7 +6,7 @@ async function main() {
   let acc = Account.fromPrivkey(getTestAccount(15).priv_key);
   console.log(getTestAccount(15).priv_key);
   let resp = await defaultClient.registerUser({
-    user_id: 0, // discard in server side
+    user_id: '0', // discard in server side
     account_id: ID.accountID[0],
     broker_id: ID.brokerID[0],
     l1_address: acc.ethAddr,
